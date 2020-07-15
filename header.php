0c3edc446bc6a143d3611909616c04e9093b7936
Aaron Lynch <aaronlynch@gmail.com>
2020-07-15 17:11:42 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,47 +1,56 @@
-<!doctype html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+<!DOCTYPE html>
+<html lang="en-US">
 
+<head itemscope itemtype="https://schema.org/WebSite">
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="referrer" content="always" />
+	<link href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.png" rel="icon" sizes="192x192" type="image/png">
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
+<body <?php body_class(); ?> itemscope itemtype="https://schema.org/WebPage">
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$_s_description = get_bloginfo( 'description', 'display' );
-			if ( $_s_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $_s_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+	<!-- Site container -->
+	<div class="site-container">
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		<!-- Skip link -->
+		<a href="#content" class="screen-reader-shortcut"> Skip to content</a>
+
+		<!-- Header -->
+		<header class="site-header" itemscope itemtype="https://schema.org/WPHeader">
+
+			<!-- Logo -->
+			<div class="site-logo" itemprop="headline">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.svg" alt="<?php bloginfo( 'name' ); ?>" title="<?php bloginfo( 'name' ); ?>">
+				</a>
+			</div>
+
+			<!-- Navigation -->
+			<nav class="site-nav" itemscope itemtype="http://www.schema.org/SiteNavigationElement">
+				<?php wp_nav_menu(
+					array(
+						'theme_location' => 'header-menu',
+						'menu_class'     => 'site-menu',
+						'menu_id'        => '',
+					)
+				); ?>
+			</nav>
+
+			<!-- Mobile Nav -->
+			<div class="mobile-menu-toggle">
+				<div class="toggle-container">
+					<span class="top"></span>
+					<span class="middle"></span>
+					<span class="bottom"></span>
+				</div>
+			</div>
+
+		</header>
+
+		<!-- Site inner -->
+		<div class="site-inner">
+
+			<!-- Content wrapper -->
+			<div class="content-wrap">
